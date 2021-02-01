7d9a6758c8074ecedad468eef268337380a02a62
Rbosca <77468875+Rbosca@users.noreply.github.com>
2021-01-31 23:53:09 -0800

    Update test-sketch.js

--- test-sketch.js
+++ test-sketch.js
@@ -1,33 +1,119 @@
+let ds;
+
 function setup() {
-  createCanvas(710, 400, WEBGL);
+  createCanvas(710, 400);
+  ds = new PenroseLSystem();
+  //please, play around with the following line
+  ds.simulate(5);
 }
 
 function draw() {
-  background(250);
-  let radius = width * 1.5;
+  background(0);
+  ds.render();
+}
 
-  //drag to move the world.
-  orbitControl();
+function PenroseLSystem() {
+    this.steps = 0;
 
-  normalMaterial();
-  translate(0, 0, -600);
-  for (let i = 0; i <= 12; i++) {
-    for (let j = 0; j <= 12; j++) {
-      push();
-      let a = (j / 12) * PI;
-      let b = (i / 12) * PI;
-      translate(
-        sin(2 * a) * radius * sin(b),
-        (cos(b) * radius) / 2,
-        cos(2 * a) * radius * sin(b)
-      );
-      if (j % 2 === 0) {
-        cone(30, 30);
-      } else {
-        box(30, 30, 30);
-      }
-      pop();
-    }
+   //these are axiom and rules for the penrose rhombus l-system
+   //a reference would be cool, but I couldn't find a good one
+    this.axiom = "[X]++[X]++[X]++[X]++[X]";
+    this.ruleW = "YF++ZF----XF[-YF----WF]++";
+    this.ruleX = "+YF--ZF[---WF--XF]+";
+    this.ruleY = "-WF++XF[+++YF++ZF]-";
+    this.ruleZ = "--YF++++WF[+ZF++++XF]--XF";
+
+    //please play around with the following two lines
+    this.startLength = 460.0;
+    this.theta = TWO_PI / 10.0; //36 degrees, try TWO_PI / 6.0, ...
+    this.reset();
+}
+
+PenroseLSystem.prototype.simulate = function (gen) {
+  while (this.getAge() < gen) {
+    this.iterate(this.production);
   }
 }
+
+PenroseLSystem.prototype.reset = function () {
+    this.production = this.axiom;
+    this.drawLength = this.startLength;
+    this.generations = 0;
+  }
+
+PenroseLSystem.prototype.getAge = function () {
+    return this.generations;
+  }
+
+//apply substitution rules to create new iteration of production string
+PenroseLSystem.prototype.iterate = function() {
+    let newProduction = "";
+
+    for(let i=0; i < this.production.length; ++i) {
+      let step = this.production.charAt(i);
+      //if current character is 'W', replace current character
+      //by corresponding rule
+      if (step == 'W') {
+        newProduction = newProduction + this.ruleW;
+      }
+      else if (step == 'X') {
+        newProduction = newProduction + this.ruleX;
+      }
+      else if (step == 'Y') {
+        newProduction = newProduction + this.ruleY;
+      }
+      else if (step == 'Z') {
+        newProduction = newProduction + this.ruleZ;
+      }
+      else {
+        //drop all 'F' characters, don't touch other
+        //characters (i.e. '+', '-', '[', ']'
+        if (step != 'F') {
+          newProduction = newProduction + step;
+        }
+      }
+    }
+
+    this.drawLength = this.drawLength * 0.5;
+    this.generations++;
+    this.production = newProduction;
+}
+
+//convert production string to a turtle graphic
+PenroseLSystem.prototype.render = function () {
+    translate(width / 2, height / 2);
+
+    this.steps += 20;
+    if(this.steps > this.production.length) {
+      this.steps = this.production.length;
+    }
+
+    for(let i=0; i<this.steps; ++i) {
+      let step = this.production.charAt(i);
+
+      //'W', 'X', 'Y', 'Z' symbols don't actually correspond to a turtle action
+      if( step == 'F') {
+        stroke(255, 60);
+        for(let j=0; j < this.repeats; j++) {
+          line(0, 0, 0, -this.drawLength);
+          noFill();
+          translate(0, -this.drawLength);
+        }
+        this.repeats = 1;
+      }
+      else if (step == '+') {
+        rotate(this.theta);
+      }
+      else if (step == '-') {
+        rotate(-this.theta);
+      }
+      else if (step == '[') {
+        push();
+      }
+      else if (step == ']') {
+        pop();
+      }
+    }
+  }
+
 
